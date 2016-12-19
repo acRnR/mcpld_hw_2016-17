0a2d81a559d0106e166eec597c0f7f7d5c02a28a
Asya Simonyan <anniesim97@gmail.com>
2016-12-19 13:18:45 +0300

    in progress

--- project4/programm.py
+++ project4/programm.py
@@ -11,28 +11,43 @@ def mystem_go():
     return outp_name
 
 
-def find_words(l):
+def find_sec(l):
     res = re.search('(.+?)\{(.+?)}', l)
     word = res.group(1)
     lemma = res.group(2)
     return word, lemma
 
 
-def make_ins(ou):
+def find_fir(l):
+
+
+
+def make_second(ou):
     fr = open(ou, 'r', encoding='UTF-8')
-    #f = fr.read()
-    raw = 'insert into one(word, lemma) values(%s, %s)\n'
-    fn = open('ins_for_db.txt',  'a', encoding='UTF-8')
+    raw = 'insert into one(id, word, lemma) values(%d, %s, %s)\n'
+    fn = open('ins_for_db.txt',  'w', encoding='UTF-8')
+    n = 0
     for line in fr:
-        w, l = find_words(line)
-        fn.write(raw % (w, l))
+        n += 1
+        w, l = find_sec(line)
+        fn.write(raw % (n, w, l))
     fr.close()
     fn.close()
+    return fn
 
+
+def make_first(fi):
+    sec = open(fi, 'r', encoding='UTF-8')
+    n = 0
+    for line in sec:
+        pass
+        n += 1
+        i, w, l = fin
+    pass
 
 def main():
     outp = mystem_go()
-    make_ins(outp)
+    make_second(outp)
 
 if __name__ == '__main__':
     main()
